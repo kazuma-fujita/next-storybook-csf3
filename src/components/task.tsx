974a37c5c9d382864ffc9a12d937763d98370c23
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-11-17 14:21:03 +0900

    Added snapshot test package.

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,18 +1,42 @@
 import React from "react";
 
-interface Props {
+type Props = {
   id: string;
   title: string;
   state: string;
   updatedAt: Date;
-  onArchiveTask?: () => void;
-  onPinTask?: () => void;
-}
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
+};
 
 export const Task = ({ id, title, state, onArchiveTask, onPinTask }: Props) => {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   );
 };
