09b8389f533e86f20c9c4af17972bce660b0279b
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-11-18 15:01:30 +0900

    Renamed task to task item.

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,38 +1,45 @@
 import React from "react";
 
-type Props = {
+export type Task = {
   id: string;
   title: string;
   state: string;
   updatedAt: Date;
+};
+
+export type Props = {
+  task: Task;
   onArchiveTask: (id: string) => void;
   onPinTask: (id: string) => void;
 };
 
-export const Task = ({ id, title, state, onArchiveTask, onPinTask }: Props) => {
+export const TaskItem = ({ task, onArchiveTask, onPinTask }: Props) => {
   return (
-    <div className={`list-item ${state}`}>
+    <div className={`list-item ${task.state}`}>
       <label className="checkbox">
         <input
           type="checkbox"
-          defaultChecked={state === "TASK_ARCHIVED"}
+          defaultChecked={task.state === "TASK_ARCHIVED"}
           disabled={true}
           name="checked"
         />
-        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(task.id)}
+        />
       </label>
       <div className="title">
         <input
           type="text"
-          value={title}
+          value={task.title}
           readOnly={true}
           placeholder="Input title"
         />
       </div>
 
       <div className="actions" onClick={(event) => event.stopPropagation()}>
-        {state !== "TASK_ARCHIVED" && (
-          <a onClick={() => onPinTask(id)}>
+        {task.state !== "TASK_ARCHIVED" && (
+          <a onClick={() => onPinTask(task.id)}>
             <span className={`icon-star`} />
           </a>
         )}
