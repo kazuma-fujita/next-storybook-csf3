b020ccb296d51bd90f231c4fccbf12344d3f166f
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-11-19 10:38:19 +0900

    Rename mock files.

--- jest.config.js
+++ jest.config.js
@@ -10,12 +10,12 @@ module.exports = {
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
 
     // Handle CSS imports (without CSS modules)
-    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/style.mock.js",
 
     /* Handle image imports
     https://jestjs.io/docs/webpack#handling-static-assets */
     "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$":
-      "<rootDir>/__mocks__/fileMock.js",
+      "<rootDir>/__mocks__/file.mock.js",
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
